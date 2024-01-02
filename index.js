f6dc36c4b521ff59c3bfd89dccbe896475647cda
Dhaval Tank <dhavaltank2001@gmail.com>
2024-01-02 17:23:35 +0530

    add utils function

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const capitalizeFirstLetter = (str) => {
             return "";
     else
         return "";
-}
+};
 
 const titleCase = (str, key = null) => {
     if (!str)
@@ -33,19 +33,166 @@ const getInitials = (name) => {
     } catch (error) {
         return "?";
     }
-}
+};
 
-const addEllipsis = (text, maxLength = 10) => {
+const getTruncateDescription = (text, maxLength = 10) => {
     if (!text)
         return "";
     else {
-        console.log('text?.length <= maxLength :>> ', text?.length, maxLength);
         if (text?.length <= maxLength) {
             return text;
         } else {
             return `${text.substring(0, maxLength)}...`;
         }
     }
-}
+};
 
-module.exports = { capitalizeFirstLetter, titleCase, getInitials, addEllipsis };
+const compareArrays = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    const sortedArr1 = arr1.slice().sort();
+    const sortedArr2 = arr2.slice().sort();
+    for (let i = 0; i < sortedArr1.length; i++) {
+        if (sortedArr1[i] !== sortedArr2[i]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const getRandomOTPWithLength = (length) => {
+    length = Math.max(1, length);
+    const min = Math.pow(10, length - 1);
+    const max = Math.pow(10, length) - 1;
+    const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomInt;
+};
+
+const slugify = (string, separator = "-") => {
+    return string
+        .toString() // Cast to string (optional)
+        .toLowerCase() // Convert the string to lowercase letters
+        .trim() // Remove whitespace from both sides of a string (optional)
+        .replace(/\s+/g, separator) // Replace spaces with -
+        .replace(/[^\w\-]+/g, "") // Remove all non-word chars
+        .replace(/\_/g, separator) // Replace _ with -
+        .replace(/\-\-+/g, separator) // Replace multiple - with single -
+        .replace(/\-$/g, ""); // Remove trailing -
+};
+
+const setLocalStorage = (key, data) => {
+    try {
+        localStorage.setItem(key, JSON.stringify(data));
+        return true;
+    } catch (error) {
+        console.error(`Error setting data in localStorage for key "${key}":`, error);
+        return false;
+    }
+};
+
+const getLocalStorage = (key) => {
+    try {
+        const storedData = localStorage.getItem(key);
+        return storedData ? JSON.parse(storedData) : null;
+    } catch (error) {
+        console.error(`Error retrieving data from localStorage for key "${key}":`, error);
+        return null;
+    }
+};
+
+const removeLocalStorage = (key) => {
+    try {
+        localStorage.removeItem(key);
+        return true;
+    } catch (error) {
+        console.error(`Error removing data from localStorage for key "${key}":`, error);
+        return false;
+    }
+};
+
+const clearLocalStorage = () => {
+    try {
+        localStorage.clear();
+        return true;
+    } catch (error) {
+        console.error("Error clearing localStorage:", error);
+        return false;
+    }
+};
+
+const getGreeting = () => {
+    const greetings = {
+        morning: "Good morning!",
+        afternoon: "Good afternoon!",
+        evening: "Good evening!",
+    };
+    const currentHour = new Date().getHours();
+    if (currentHour >= 5 && currentHour < 12)
+        return greetings.morning;
+    else
+        if (currentHour >= 12 && currentHour < 17)
+            return greetings.afternoon;
+        else
+            return greetings.evening;
+};
+
+const validateText = (value, errorMessage = null) => {
+    if (!value?.trim())
+        return { isError: true, errorMessage: (errorMessage || "This field must not be empty.") };
+    const regex = /^[a-zA-Z ]+$/;
+    if (!regex.test(value?.trim())) {
+        return { isError: true, errorMessage: (errorMessage || "Invalid input") };
+    }
+    return { isError: false, errorMessage: "Valid input" };
+};
+
+const validateEmail = (email, errorMessage = null) => {
+    if (!email?.trim())
+        return { isError: true, errorMessage: (errorMessage || "This field must not be empty.") };
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(email?.trim())) {
+        return { isError: true, errorMessage: (errorMessage || "Invalid email") };
+    }
+    return { isError: false, errorMessage: "Valid email" };
+};
+
+const validatePassword = (password, errorMessage = null) => {
+    if (!password?.trim())
+        return { isError: true, errorMessage: (errorMessage || "This field must not be empty.") };
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?]).{8,40}$/;
+    if (!regex.test(password?.trim())) {
+        return { isError: true, errorMessage: (errorMessage || "Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one symbol.") };
+    }
+    return { isError: false, errorMessage: "Valid password" };
+};
+
+const validateMobileNumber = (value, errorMessage = null) => {
+    if (!value?.trim())
+        return { isError: true, errorMessage: (errorMessage || "This field must not be empty.") };
+    const regex = /^\d{10}$/;
+    if (!regex.test(value?.trim())) {
+        return { isError: true, errorMessage: (errorMessage || "Invalid mobile number") };
+    }
+    return { isError: false, errorMessage: "Valid mobile number" };
+};
+
+
+module.exports = {
+    capitalizeFirstLetter,
+    titleCase,
+    getInitials,
+    getTruncateDescription,
+    compareArrays,
+    getRandomOTPWithLength,
+    slugify,
+    setLocalStorage,
+    getLocalStorage,
+    removeLocalStorage,
+    clearLocalStorage,
+    getGreeting,
+    validateText,
+    validateEmail,
+    validatePassword,
+    validateMobileNumber,
+};
--- index.test.js
+++ index.test.js
@@ -1,5 +1,18 @@
 // myFunction.test.js
-const { capitalizeFirstLetter, titleCase, getInitials, addEllipsis } = require('./index.js');
+const { test, expect } = require('@jest/globals');
+const {
+    capitalizeFirstLetter,
+    titleCase,
+    getInitials,
+    compareArrays,
+    getRandomOTPWithLength,
+    slugify,
+    getTruncateDescription,
+    validateText,
+    validateEmail,
+    validatePassword,
+    validateMobileNumber
+} = require('./index.js');
 
 // test case for capitalizeFirstLetter function
 describe('Check capitalizeFirstLetter function', () => {
@@ -70,14 +83,115 @@ describe('check getInitials function', () => {
     });
 });
 
-describe('check addEllipsis function', () => {
+describe('check getTruncateDescription function', () => {
     test('check with string', () => {
-        const result = addEllipsis("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, a.");
-        expect(result).toBe("Lorem ipsu...", 10)
+        const result = getTruncateDescription("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, a.");
+        console.log("🚀 ~ file: index.test.js:89 ~ test ~ result:", result)
+        expect(result).toBe("Lorem ipsu...");
     })
 
     test('check with string which have length 20', () => {
-        const result = addEllipsis("Lorem ipsum", 20);
-        expect(result).toBe("Lorem ipsum")
+        const result = getTruncateDescription("Lorem ipsum", 5);
+        expect(result).toBe("Lorem...");
     })
+});
+
+describe("check compareArrays function", () => {
+    test('check array', () => {
+        let result = compareArrays([1, 2, 3], [3, 1, 2]);
+        expect(result).toBe(true);
+    })
+
+    test('check array', () => {
+        let result = compareArrays([1, 2, 3], [4, 1, 2]);
+        expect(result).toBe(false);
+    })
+})
+
+describe("check getRandomOTPWithLength function", () => {
+    test('should generate a random number with the specified length', () => {
+        const length = 4;
+        const randomNumber = getRandomOTPWithLength(length);
+        expect(typeof randomNumber).toBe('number');
+        expect(randomNumber).toBeGreaterThanOrEqual(Math.pow(10, length - 1));
+        expect(randomNumber).toBeLessThanOrEqual(Math.pow(10, length) - 1);
+        expect(randomNumber.toString().length).toBe(length);
+    });
+
+    test('should handle edge cases', () => {
+        const minLength = 1;
+        const minNumber = getRandomOTPWithLength(minLength);
+        expect(minNumber.toString().length).toBe(minLength);
+
+        const largeLength = 7;
+        const largeNumber = getRandomOTPWithLength(largeLength);
+        expect(largeNumber.toString().length).toBe(largeLength);
+    });
+})
+
+describe('check slugify function', () => {
+    test('converts a string to a slug with default separator', () => {
+        const result = slugify('Hello World');
+        expect(result).toBe('hello-world');
+    });
+
+    test('converts a string to a slug with custom separator', () => {
+        const result = slugify('Hello World', '_');
+        expect(result).toBe('hello_world');
+    });
+
+    test('handles special characters and multiple spaces', () => {
+        const result = slugify('  This is $%^a   test!  ');
+        expect(result).toBe('this-is-a-test');
+    });
+
+    test('handles underscores and trailing dash', () => {
+        const result = slugify('Some_Text_With-Trailing-Dash-');
+        expect(result).toBe('some-text-with-trailing-dash');
+    });
+});
+
+describe('Validation Functions', () => {
+    test('validateText should return valid for non-empty alphabetic text', () => {
+        const result = validateText('John Doe');
+        expect(result).toEqual({ isError: false, errorMessage: 'Valid input' });
+    });
+
+    test('validateText should return error for empty text', () => {
+        const result = validateText('');
+        expect(result).toEqual({ isError: true, errorMessage: 'This field must not be empty.' });
+    });
+
+    test('validateEmail should return valid for a valid email', () => {
+        const result = validateEmail('test@example.com');
+        expect(result).toEqual({ isError: false, errorMessage: 'Valid email' });
+    });
+
+    test('validateEmail should return error for an invalid email', () => {
+        const result = validateEmail('invalid-email');
+        expect(result).toEqual({ isError: true, errorMessage: 'Invalid email' });
+    });
+
+    test('validatePassword should return valid for a valid password', () => {
+        const result = validatePassword('Abc@1234');
+        expect(result).toEqual({ isError: false, errorMessage: 'Valid password' });
+    });
+
+    test('validatePassword should return error for an invalid password', () => {
+        const result = validatePassword('weakpassword');
+        expect(result).toEqual({
+            isError: true,
+            errorMessage: 'Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one symbol.'
+        });
+    });
+
+    test('validateMobileNumber should return valid for a valid mobile number', () => {
+        const result = validateMobileNumber('1234567890');
+        expect(result).toEqual({ isError: false, errorMessage: 'Valid mobile number' });
+    });
+
+    test('validateMobileNumber should return error for an invalid mobile number', () => {
+        const result = validateMobileNumber('invalidnumber');
+        expect(result).toEqual({ isError: true, errorMessage: 'Invalid mobile number' });
+    });
 });
